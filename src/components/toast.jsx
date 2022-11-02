8d6b74aff731d285d6fce51d4e4ee120b26aba09
ricardo4466 <rteixeira@mkmservice.com>
2022-11-02 14:37:39 -0300

    structuring lading page with tailwindcss and conexion off api

---
+++ src/components/toast.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Toast() {
+  const notify = () => toast("Wow so easy!");
+
+  return (
+    <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+}
